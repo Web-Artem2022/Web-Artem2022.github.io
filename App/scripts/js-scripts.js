ed6e744573cd9985b50847e03f850edde4bd9bfc
Web-Artem2022 <116104803+Web-Artem2022@users.noreply.github.com>
2022-10-18 20:00:06 +0300

    Add files via upload

---
+++ App/scripts/js-scripts.js
@@ -0,0 +1,22 @@
+function superClick(el) {
+			el.style.background ="yellow";
+			el.style.color = 'blue';
+		}
+
+var h3Showed=false;
+
+
+
+
+
+
+
+function changeDisplay(){
+	const header3 =document.getElementById("1")
+	header3.style.display=h3Showed ? 'block': 'none';
+	h3Showed= !h3Showed
+
+}
+
+
+
